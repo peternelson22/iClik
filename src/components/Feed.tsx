8077e99e44887bed3e1ad527532707133663c007
nelson <peternelson063@gmail.com>
2023-07-10 21:45:05 +0100

    Made the Feed section responsive

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -4,13 +4,12 @@ import Stories from './Stories';
 type Props = {};
 const Feed = (props: Props) => {
   return (
-    <main>
-      <section>
+    <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
+      <section className='md:col-span-2 '>
         <Stories />
-      </section>
-      <section>
         <Posts />
       </section>
+      <section className='hidden  md:inline-grid md:col-span-1'></section>
     </main>
   );
 };
