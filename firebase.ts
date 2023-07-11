c35927388f4267ac5dd6a96bae04625e8d9a858a
nelson <peternelson063@gmail.com>
2023-07-11 16:40:34 +0100

    Added firebase and next-auth

---
+++ firebase.ts
@@ -0,0 +1,19 @@
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: 'iclik-ng',
+  storageBucket: 'iclik-ng.appspot.com',
+  messagingSenderId: process.env.FIREBASE_MESSAGE_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
